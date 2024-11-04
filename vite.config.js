875703d9704d579b91824fc961150b9ccc45e79f
Ibrahim Saleh <ibr.sale7@gmail.com>
2024-11-04 12:27:56 +0300

    viteconfig chunkSize

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Create separate chunks for libraries or specific parts of your application
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+      plugins: [
+        visualizer({
+          filename: 'stats.html',
+          open: true, // Automatically open the report in the browser
+        }),
+      ],
+    },
+  },
 })
