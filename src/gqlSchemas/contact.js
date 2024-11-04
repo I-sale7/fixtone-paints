0f7301822c173464e8544fd2b6baf4ae55f96c76
I-sale7 <I-sale7@users.noreply.github.com>
2024-11-04 11:44:23 +0000

    deploy: 8008803528147464b75e598ebae2fb1bc4335606

--- src/gqlSchemas/contact.js
+++
@@ -1,9 +0,0 @@
-export const contactQuery = (locale = "en-US")=> `{
-  contactCollection(order: id_ASC, locale: "${locale}"){
-    items{
-      id
-      title
-      description
-    }
-  }
-}`
--- src/gqlSchemas/index.js
+++
@@ -1,3 +0,0 @@
-export { aboutQuery as homeAboutComponentCollection } from "./about";
-export { bannerQuery as heroBannerCollection } from "./banner";
-export { productsQuery } from "./products";
